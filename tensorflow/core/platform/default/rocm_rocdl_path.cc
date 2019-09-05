4b9548a773f405f33f2c306d548d4f41096e7073
jerryyin <zhuoryin@amd.com>
2019-09-05 20:59:54 +0000

    Fixing rocm_rocdl include path

--- tensorflow/core/platform/default/rocm_rocdl_path.cc
+++ tensorflow/core/platform/default/rocm_rocdl_path.cc
@@ -18,7 +18,7 @@ limitations under the License.
 #include <stdlib.h>
 
 #if !defined(PLATFORM_GOOGLE) && TENSORFLOW_USE_ROCM
-#include "third_party/gpus/rocm/rocm_config.h"
+#include "rocm/rocm_config.h"
 #endif
 #include "tensorflow/core/platform/logging.h"
 
